db37be25e54d0f8bc20d240084d293b9ef70bb0a
Rezvano <ivano08.01.1998@gmail.com>
2022-03-14 07:50:55 +0300

    Нарисована шахматная доска средствами движка

--- libs/Object/Object.cpp
+++ libs/Object/Object.cpp
@@ -2,17 +2,16 @@
 #include "Object.h"
 
 Object::Object(double x, double y, double width, double height, Mesh *mesh, float *color) : x(x),
-                                                                                                y(y),
-                                                                                                width(width),
-                                                                                                height(height),
-                                                                                                mesh(mesh),
-                                                                                                color(color)
+                                                                                            y(y),
+                                                                                            width(width),
+                                                                                            height(height),
+                                                                                            mesh(mesh),
+                                                                                            color(color)
 {
 }
 
 void Object::onClickAnyWhere(double clickX, double clickY)
 {
-    //std::cout << "CLICK AT X: " << clickX << " Y: " << clickY << std::endl;
     if (clickX > x && clickX < (x + width) && clickY > y && clickY < (y + height))
     {
         onClick();
--- libs/Object/Object.h
+++ libs/Object/Object.h
@@ -5,12 +5,11 @@
 class Object
 {
 public:
-    Object(double x, double y, double width, double height, Mesh *mesh, float *color);
     double x, y, width, height;
-
     Mesh *mesh;
     float *color;
 
+    Object(double x, double y, double width, double height, Mesh *mesh, float *color);
     virtual void onClickAnyWhere(double clickX, double clickY);
     virtual void onClick();
 };
